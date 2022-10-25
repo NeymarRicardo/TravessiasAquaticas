7781d44756dab53edf19b828a4c3d42a143e6d3e
NeymarRicardo <66969183+NeymarRicardo@users.noreply.github.com>
2022-10-25 00:51:41 -0300

    Atualização do script

--- script.php
+++ script.php
@@ -30,4 +30,32 @@ $modalidade = $_POST['modalidade'];
 	}
 	else
 		echo "Idade da/o atetla: ". $idade . "</br>"; 
+	if($sexo === 'feminino')
+	{
+		echo "Sexo da atetla é feminino <br/>";
+	}
+	else
+	 {
+		echo "Sexo do atetla é masculino <br/>";
+	}
+	if($distancia === '1')
+	{
+		echo "A distância selecionada é de 1 KM <br/>";
+	}
+	else 
+	{
+		echo "A distância escolhida é de 3 KM <br/>";
+	}
+	if($modalidade === 'convencional')
+	{
+		echo "A modalidade escolhida é: Convencional <br/>";
+	}
+	elseif ($modalidade === 'on')
+	 {
+		echo "A modalidade escolhida é: Óculos e Nadadeira (ON) <br/>";
+	}
+	else
+	{
+		echo "A modalidade escolhida é: Máscara, Respirador e Nadadeira (MRN) <br/>";
+	}
 ?>
